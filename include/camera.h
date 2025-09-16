f6a0593f555a639803db10575b7ed45eeb5a3bc5
Walter Goodzey <145727171+wgoodzey@users.noreply.github.com>
2025-09-16 15:40:02 -0500

    addded sampling

--- include/camera.h
+++ include/camera.h
@@ -8,6 +8,7 @@ class camera {
  public:
   double aspect_ratio = 1.0;
   int image_width = 100;
+  int samples_per_pixel = 10;
 
   void render(const hittable& world) {
     initialize();
@@ -18,14 +19,13 @@ class camera {
 
     for (int j = 0; j < image_height; j++) {
       for (int i = 0; i < image_width; i++) {
-        auto pixel_center =
-            pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-        auto ray_direction = pixel_center - center;
-        ray r(center, ray_direction);
+        color<double> pixel_color(0.0, 0.0, 0.0);
+        for (int sample = 0; sample < samples_per_pixel; sample++) {
+          ray r = get_ray(i, j);
+          pixel_color += ray_color(r, world);
+        }
 
-        color<double> pixel_color = ray_color(r, world);
-
-        raster[j][i] = color_out(pixel_color);
+        raster[j][i] = color_out(pixel_sample_scale * pixel_color);
       }
     }
 
@@ -38,6 +38,7 @@ class camera {
 
  private:
   int image_height;
+  double pixel_sample_scale;
   point3<double> center;
   point3<double> pixel00_loc;
   vec3<double> pixel_delta_u;
@@ -46,6 +47,8 @@ class camera {
   void initialize() {
     image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    pixel_sample_scale = 1.0 / samples_per_pixel;
 
     center = point3<double>(0.0, 0.0, 0.0);
 
@@ -63,6 +66,21 @@ class camera {
     auto viewport_upper_left = center - vec3<double>(0.0, 0.0, focal_length) -
                                viewport_u / 2 - viewport_v / 2;
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+  }
+
+  ray get_ray(int i, int j) const {
+    auto offset = sample_square();
+    auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) +
+                        ((j + offset.y()) * pixel_delta_v);
+
+    auto ray_origin = center;
+    auto ray_direction = pixel_sample - ray_origin;
+
+    return ray(ray_origin, ray_direction);
+  }
+
+  vec3<double> sample_square() const {
+    return vec3<double>(random_double() - 0.5, random_double() - 0.5, 0);
   }
 
   color<double> ray_color(const ray& r, const hittable& world) const {
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 int main(int argc, char* argv[]) {
   hittable_list world;
+
   world.add(std::make_shared<sphere>(point3<double>(0, 0, -1), 0.5));
   world.add(std::make_shared<sphere>(point3<double>(0, -100.5, -1), 100));
 
@@ -16,6 +17,7 @@ int main(int argc, char* argv[]) {
 
   cam.aspect_ratio = 16.0 / 9.0;
   cam.image_width = 400;
+  cam.samples_per_pixel = 100;
 
   cam.render(world);
 
