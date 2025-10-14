478ade0af2ecbe190c30668bfdab4432ef49d5e3
Walter Goodzey <145727171+wgoodzey@users.noreply.github.com>
2025-10-14 17:13:49 -0500

    finished dielectrics from chapter 11 of book 1

--- include/material.h
+++ include/material.h
@@ -63,12 +63,28 @@ class dielectric : public material {
     double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
 
     vec3<double> unit_direction = unit_vector(r_in.direction());
-    vec3<double> refracted = refract(unit_direction, rec.normal, ri);
+    double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+    double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
 
-    scattered = ray(rec.p, refracted);
+    bool cannot_refract = ri * sin_theta > 1.0;
+    vec3<double> direction;
+
+    if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
+      direction = reflect(unit_direction, rec.normal);
+    } else {
+      direction = refract(unit_direction, rec.normal, ri);
+    }
+
+    scattered = ray(rec.p, direction);
     return true;
   }
 
  private:
   double refraction_index;
+
+  static double reflectance(double cosine, double refraction_index) {
+    auto r0 = (1 - refraction_index) / (1 + refraction_index);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+  }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,14 @@ int main(int argc, char* argv[]) {
   auto material_ground = make_shared<lambertian>(color<double>(0.8, 0.8, 0.0));
   auto material_center = make_shared<lambertian>(color<double>(0.1, 0.2, 0.5));
   auto material_left = make_shared<dielectric>(1.50);
+  auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
   auto material_right = make_shared<metal>(color<double>(0.8, 0.6, 0.2), 1.0);
 
-  world.add(make_shared<sphere>(point3<double>(0.0, -100.5, -1.0), 100.0,
-                                material_ground));
-  world.add(make_shared<sphere>(point3<double>(0.0, 0.0, -1.2), 0.5,
-                                material_center));
-  world.add(
-      make_shared<sphere>(point3<double>(-1.0, 0.0, -1.0), 0.5, material_left));
-  world.add(
-      make_shared<sphere>(point3<double>(1.0, 0.0, -1.0), 0.5, material_right));
+  world.add(make_shared<sphere>(point3<double>(0.0, -100.5, -1.0), 100.0, material_ground));
+  world.add(make_shared<sphere>(point3<double>(0.0, 0.0, -1.2), 0.5, material_center));
+  world.add(make_shared<sphere>(point3<double>(-1.0, 0.0, -1.0), 0.5, material_left));
+  world.add(make_shared<sphere>(point3<double>(-1.0, 0.0, -1.0), 0.4, material_bubble));
+  world.add(make_shared<sphere>(point3<double>(1.0, 0.0, -1.0), 0.5, material_right));
 
   camera cam;
 
