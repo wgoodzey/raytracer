9939591f773ccc25dcc3bca800d437a999bb75e9
Walter Goodzey <145727171+wgoodzey@users.noreply.github.com>
2025-10-28 18:42:51 -0500

    added switch to change between different renders

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,11 @@
 #include "sphere.h"
 #include "texture.h"
 
-int main(int argc, char* argv[]) {
+void bouncing_spheres() {
   hittable_list world;
 
-  auto checker = make_shared<checker_texture>(0.32, color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
-  world.add(make_shared<sphere>(point3(0.0, -1000, 0.0), 1000, make_shared<lambertian>(checker)));
+  auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));;
+  world.add(make_shared<sphere>(point3(0.0, -1000.0, 0.0), 1000.0, ground_material));
 
   for (int a = -11; a < 11; a++) {
     for (int b = -11; b < 11; b++) {
@@ -28,12 +28,12 @@ int main(int argc, char* argv[]) {
           // diffuse
           auto albedo = color::random() * color::random();
           sphere_material = make_shared<lambertian>(albedo);
-          auto center2 = center + vec3<double>(0.0, random_double(0.0, 0.5), 0);
+          auto center2 = center + vec3<double>(0.0, random_double(0.0, 0.5), 0.0);
           world.add(make_shared<sphere>(center, center2, 0.2, sphere_material));
         } else if (choose_mat < 0.95) {
           // metal
-          auto albedo = color::random(0.5, 1);
-          auto fuzz = random_double(0, 0.5);
+          auto albedo = color::random(0.5, 1.0);
+          auto fuzz = random_double(0.0, 0.5);
           sphere_material = make_shared<metal>(albedo, fuzz);
           world.add(make_shared<sphere>(center, 0.2, sphere_material));
         } else {
@@ -46,13 +46,13 @@ int main(int argc, char* argv[]) {
   }
 
   auto material1 = make_shared<dielectric>(1.5);
-  world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+  world.add(make_shared<sphere>(point3(0.0, 1.0, 0.0), 1.0, material1));
 
   auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
   world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
   auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-  world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+  world.add(make_shared<sphere>(point3(4.0, 1.0, 0.0), 1.0, material3));
 
   world = hittable_list(make_shared<bvh_node>(world));
 
@@ -72,6 +72,40 @@ int main(int argc, char* argv[]) {
   cam.focus_dist    = 10.0;
 
   cam.render(world);
+}
 
+void checkered_spheres() {
+  hittable_list world;
+
+  auto checker = make_shared<checker_texture>(0.32, color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+
+  world.add(make_shared<sphere>(point3(0.0, -10.0, 0.0), 10.0, make_shared<lambertian>(checker)));
+  world.add(make_shared<sphere>(point3(0.0,  10.0, 0.0), 10.0, make_shared<lambertian>(checker)));
+
+  world = hittable_list(make_shared<bvh_node>(world));
+
+  camera cam;
+
+  cam.aspect_ratio      = 16.0 / 9.0;
+  cam.image_width       = 400;
+  cam.samples_per_pixel = 100;
+  cam.max_depth         = 50;
+
+  cam.vfov     = 20;
+  cam.lookfrom = point3(13.0, 2.0, 3.0);
+  cam.lookat   = point3(0.0, 0.0, 0.0);
+  cam.vup      = vec3<double>(0.0, 1.0, 0.0);
+
+  cam.defocus_angle = 0.6;
+  cam.focus_dist    = 10.0;
+
+  cam.render(world);
+}
+
+int main(int argc, char* argv[]) {
+  switch (2) {
+    case 1: bouncing_spheres(); break;
+    case 2: checkered_spheres(); break;
+  }
   return 0;
 }
